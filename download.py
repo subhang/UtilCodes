1bbd3c630e885ac989d69db684dc1fa5cd442b19
Kushal Santhosh <kushal.roxx10@gmail.com>
2015-11-17 18:55:34 +0530

    Update download.py

--- download.py
+++ download.py
@@ -5,16 +5,16 @@ import sys
 import codecs
 from bs4 import BeautifulSoup
 url = "https://www.youtube.com"
-link = ""
 ext = "/results?search_query="
 query = sys.argv[1]
 name = query.replace("+","_")
 a = urllib2.urlopen(url+ext+query).read()
 soup = BeautifulSoup(a)
-for i in soup.findAll("a",{"class":" yt-uix-sessionlink     spf-link "}):
+for i in soup.findAll("a",{"class":"yt-uix-sessionlink yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2       spf-link "}):
     link =  url+i["href"]
     break
-os.system('youtube-dl -o "'+name+'.m4a" --extract-audio '+a)
+print "This is The link"+link
+os.system('youtube-dl -o "'+name+'.m4a" --extract-audio '+link)
 if(len(sys.argv) == 3):
   if(sys.argv[2] == "play"):
     os.system("mplayer "+name+".m4a")
